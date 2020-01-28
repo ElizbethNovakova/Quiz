e8a2c1ab331c64c9a06fe7926114cd532ed680a9
ElizbethNovakova <lizochka1211@gmail.com>
2020-01-28 13:01:00 +0200

    adaptive

--- app/src/main/java/com/lizoknovakova/quiz/Level1.java
+++ app/src/main/java/com/lizoknovakova/quiz/Level1.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+
+        final ImageView imageLeft = findViewById(R.id.imageLeft);
+        imageLeft.setClipToOutline(true); //скруглим углы
+        final ImageView imageRight = findViewById(R.id.imageRight);
+        imageRight.setClipToOutline(true); //скруглим углы
 
         Window w = getWindow(); //убрали строку состояния(заряд и тд)
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
