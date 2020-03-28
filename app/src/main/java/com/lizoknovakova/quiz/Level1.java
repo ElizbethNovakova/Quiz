1e3b4a2ff630bfafb3523b7b4a878b78d1c15f22
ElizbethNovakova <lizochka1211@gmail.com>
2020-03-28 16:36:05 +0200

    New preview

--- app/src/main/java/com/lizoknovakova/quiz/Level1.java
+++ app/src/main/java/com/lizoknovakova/quiz/Level1.java
@@ -1,12 +1,23 @@
 package com.lizoknovakova.quiz;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.logging.Level;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,66 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow(); //убрали строку состояния(заряд и тд)
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Вызов диалогового окна в начале игры
+        dialog= new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрываем заголовок
+        dialog.setContentView(R.layout.previewdialog);//путь к макету диалогового окна
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//прозрачный фон диалогового окна
+        dialog.setCancelable(false);//окно нельзя закріть кнопкой назад
+
+
+        //кнопка которая закрывает диалоговое окно
+        TextView buttonclose = dialog.findViewById(R.id.buttonclose);
+        buttonclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //обрабатываем нажатие кнопки
+                try{
+                    //вернуться к вібору уровня
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e){
+
+                }
+                dialog.dismiss();//закрываем диалоговое окно
+            }
+        });
+
+        Button button_continue = dialog.findViewById(R.id.button_continue);
+        button_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();//закрываем диалоговое окно
+            }
+        });
+
+        dialog.show();//показать диалоговое окно
+
+        //Кнопка назад
+        Button button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public  void onBackPressed(){
+        try{
+            Intent intent = new Intent(Level1.this, GameLevels.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+        }
     }
 }
