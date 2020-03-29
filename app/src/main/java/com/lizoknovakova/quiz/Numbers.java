ce2e9d09cd925a5cae79b68ece13c590aefc7f9d
ElizbethNovakova <lizochka1211@gmail.com>
2020-03-29 16:49:36 +0300

    Level1 is finished

---
+++ app/src/main/java/com/lizoknovakova/quiz/Numbers.java
@@ -0,0 +1,31 @@
+package com.lizoknovakova.quiz;
+
+public class Numbers {
+    //array for first level
+
+    final int[] images = {
+            R.drawable.zero,
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six,
+            R.drawable.seven,
+            R.drawable.eight,
+            R.drawable.nine
+    };
+
+    final int[] texts = {
+            R.string.level1_zero,
+            R.string.level1_one,
+            R.string.level1_two,
+            R.string.level1_three,
+            R.string.level1_four,
+            R.string.level1_five,
+            R.string.level1_six,
+            R.string.level1_seven,
+            R.string.level1_eight,
+            R.string.level1_nine
+    };
+}
